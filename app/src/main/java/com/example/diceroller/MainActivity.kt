9726552e83e1b79bfccff8aa37379ee5a4791321
jeremyzhli <jeremyzh.li@gmail.com>
2025-07-22 16:25:44 +1000

    add d20 and d6 roller and display result

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+private const val INIT_MODIFIER: Int = 0
+
 class MainActivity : AppCompatActivity() {
+    private var modifier: Int = INIT_MODIFIER
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val rollResult: TextView = findViewById(R.id.rollResult)
+
+        val buttonD20: ImageButton = findViewById(R.id.button_d20)
+        buttonD20.setOnClickListener {
+            val result = rollDie(20) + modifier
+            rollResult.text = result.toString()
+        }
+
+        val buttonD6: ImageButton = findViewById(R.id.button_d6)
+        buttonD6.setOnClickListener {
+            val result = rollDie(6) + modifier
+            rollResult.text = result.toString()
+        }
+    }
+
+    private fun rollDie(sides: Int): Int {
+        return (1..sides).random()
     }
 }
